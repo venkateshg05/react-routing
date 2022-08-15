0109c0cbadef630082275f29a1c3c5821447bbf3
venkateshg05 <venkatesh_gopal@outlook.com>
2022-08-14 22:56:09 -0500

    (feat) add pages for routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import { Route } from "react-router-dom";
+import MainHeader from "./components/MainHeader";
+import Welcome from "./pages/Welcome";
+import Products from "./pages/Products";
+
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <MainHeader />
+      <Route path={"/welcome"}>
+        <Welcome />
+      </Route>
+      <Route path={"/products"}>
+        <Products />
+      </Route>
     </div>
   );
 }
---
+++ src/pages/Welcome.js
@@ -0,0 +1,9 @@
+const Welcome = () => {
+  return (
+    <div>
+      <h1>Welcome page</h1>
+    </div>
+  );
+};
+
+export default Welcome;
